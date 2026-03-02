e3182afb0d2f9319797483e4c0b891019c8c96c7
ThariX07 <contact.tharindu007@gmail.com>
2026-03-02 22:08:08 +0530

    basicSystem

--- src/main/java/com/oceanview/controller/BillServlet.java
+++ src/main/java/com/oceanview/controller/BillServlet.java
@@ -1,6 +1,10 @@
 package com.oceanview.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import com.oceanview.dao.ReservationDAO;
 import com.oceanview.dto.BillDTO;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +13,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +22,22 @@ import java.util.Map;
 public class BillServlet extends HttpServlet {
 
     private final ReservationDAO reservationDAO = new ReservationDAO();
-    private final Gson gson = new Gson();
+
+    private final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDate.class, new TypeAdapter<LocalDate>() {
+                @Override
+                public void write(JsonWriter jsonWriter, LocalDate localDate) throws IOException {
+                    if (localDate == null) {
+                        jsonWriter.nullValue();
+                    } else {
+                        jsonWriter.value(localDate.format(DateTimeFormatter.ISO_LOCAL_DATE));
+                    }
+                }
+                @Override
+                public LocalDate read(JsonReader jsonReader) throws IOException {
+                    return LocalDate.parse(jsonReader.nextString(), DateTimeFormatter.ISO_LOCAL_DATE);
+                }
+            }).create();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -42,9 +63,8 @@ public class BillServlet extends HttpServlet {
             jsonResponse.put("status", "success");
             jsonResponse.put("bill", bill);
         } else {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             jsonResponse.put("status", "error");
-            jsonResponse.put("message", "Reservation not found.");
+            jsonResponse.put("message", "Reservation not found in the database.");
         }
 
         out.print(gson.toJson(jsonResponse));
